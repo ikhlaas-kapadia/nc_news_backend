dfbb07ee135fff88e1529fc23ea4d110c7424d33
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-03-13 09:50:26 +0000

    Updated heroku configuration, few errors

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,11 @@
+const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
-const dbConfig = require("../knexfile");
-const connection = knex(dbConfig);
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
+
+module.exports = knex(dbConfig);
+// const connection = knex(dbConfig);
 // In knex it creates connection with databse automatically but in other files we need to create the connection
-module.exports = connection;
+// module.exports = connection;
