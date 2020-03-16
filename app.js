73cf2150c63913707173e54604d6117910fe709c
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-03-16 15:01:39 +0000

    Added cors variable to prevent hosting issues

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const apiRouter = require("./routers/api-router");
 const {
   handle404s,
   handleCustomError,
   handlePsqlError
 } = require("./errors/errors");
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", apiRouter);
