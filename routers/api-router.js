feb18d27a43cb789eb10ff0ccd5a144469bbcbf3
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-04-21 09:46:58 +0100

    Added endpoint get request to router

--- routers/api-router.js
+++ routers/api-router.js
@@ -3,12 +3,13 @@ const apiRouter = require("express").Router();
 const userRouter = require("./users-router");
 const articlesRouter = require("./articles-router");
 const commentsRouter = require("./comments-router");
+const { getEndpoints } = require("../controllers/endpoints-controller");
 const { handle405s } = require("../errors/errors");
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", userRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
-apiRouter.route("/").all(handle405s);
+apiRouter.route("/").get(getEndpoints).all(handle405s);
 
 module.exports = apiRouter;
